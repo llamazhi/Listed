8f95e65de4fa513892b99c9818756c8f4cdbb7a5
llamazhi <zhongz22@uw.edu>
2025-02-19 15:44:50 -0800

    Added more setup files

---
+++ src/popup/Popup.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+
+import './Popup.css'
+
+export const Popup = () => {
+  return (
+    <main>
+      <h3>Popup Page</h3>
+    </main>
+  )
+}
+
+export default Popup
